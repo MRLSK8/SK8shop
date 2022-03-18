5ab304aa2e63a0d4a987ef61bd95b7eef78ed067
Marcelo Lima <jmdl.k8@gmail.com>
2022-03-18 16:42:34 -0300

    feat: add config in order to use firestore emulator

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import 'react-native-gesture-handler';
-import { LogBox } from 'react-native';
-import { AppRegistry } from 'react-native';
+import '@react-native-firebase/app';
+
+import firestore from '@react-native-firebase/firestore';
+import { LogBox, AppRegistry } from 'react-native';
+
 import App from './src';
-import { name as appName } from './app.json';
+import {
+  name as appName
+} from './app.json';
+
+// uncomment this to use firestore emulator
+// if (__DEV__) {
+//   firestore().useEmulator('localhost', 8080);
+// }
 
 LogBox.ignoreLogs(['Require cycle:']);
 
