4485b3bb678f8a3d290d2a94d929768896292459
Marcelo Lima <jmdl.k8@gmail.com>
2022-08-11 21:38:54 -0300

    chore: update react native to 0.68.3

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import {
   name as appName
 } from './app.json';
 
-// uncomment this to use firestore emulator
+// Uncomment this to use firestore emulator
 // if (__DEV__) {
 //   firestore().useEmulator('localhost', 8080);
 // }
