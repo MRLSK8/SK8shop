3a9622f1913e83df7fad4229c9fe3008b07fa4c7
Marcelo Lima <jmdl.k8@gmail.com>
2022-08-13 10:32:02 -0300

    chore: add comments explaning about the base url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://reqres.in/api/'
+  // Website for more details about this api: https://reqres.in/
+  baseURL: 'https://reqres.in/api/' // base url for testing api calls. 
 });
 
 export default api;
