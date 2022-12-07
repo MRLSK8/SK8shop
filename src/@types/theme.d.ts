374de7d350c4b279f1630e4403ceab3143671a95
Marcelo Lima <jose.lima@compasso.com.br>
2022-12-07 16:00:01 -0300

    fix: fix bug styled components not recognizing types

--- src/@types/theme.d.ts
+++ src/@types/theme.d.ts
@@ -1,30 +1,9 @@
 import 'styled-components';
 
-declare module 'styled-components' {
-  export interface DefaultTheme {
-    colors: {
-      primary: string,
+import theme from "~/styles/globalStyles";
 
-      secondary: string,
+export type Theme = typeof theme;
 
-      tertiary: string,
-
-      white: string,
-      white100: string,
-      white200: string,
-      white300: string,
-
-      dark: string,
-      dark100: string,
-      dark200: string,
-    },
-    fonts: {
-      Ubuntu: {
-        BoldItalic: string,
-        Regular: string,
-        Italic: string,
-        Bold: string,
-      }
-    }
-  }
+declare module 'styled-components/native' {
+	export interface DefaultTheme extends Theme { }
 }
