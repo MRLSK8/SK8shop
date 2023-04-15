ed0917e02b27a4e5debaa7fa6d434bf4fc377279
Marcelo Lima <jose.lima@compasso.com.br>
2023-04-15 11:31:21 -0300

    fix: set statusbar style to light-content

--- src/config/statusBarConfig.ts
+++ src/config/statusBarConfig.ts
@@ -1,5 +1,7 @@
 import { StatusBar, Platform } from 'react-native';
 
 if (Platform.OS === "android") {
-    StatusBar.setBackgroundColor('#171c2b');
+	StatusBar.setBackgroundColor('#171c2b');
 }
+
+StatusBar.setBarStyle('light-content');
