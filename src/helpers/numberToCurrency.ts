e5507a1f083b6ea72ea93880a54c025793b437aa
Marcelo Lima <jose.lima@compasso.com.br>
2023-04-15 11:19:21 -0300

    fix: fix bug when numberToCurrency is called with 0 as a paramenter

--- src/helpers/numberToCurrency.ts
+++ src/helpers/numberToCurrency.ts
@@ -1,5 +1,5 @@
 export const numberToCurrency = (value: number) => {
-	if (!value) return '';
+	if (!value) return 'R$ 0,00';
 
 	return `R$ ${value.toFixed(2).replace('.', ',').replace(/(\d)(?=(\d{3})+\,)/g, '$1.')}`;
 }
