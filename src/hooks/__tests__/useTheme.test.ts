33a39660ab98ad4dfd15910f9b5218d583ec4687
Marcelo Lima <jose.lima@compasso.com.br>
2023-04-28 14:56:19 -0300

    test: make some improviments on the useTheme tests

--- src/hooks/__tests__/useTheme.test.ts
+++ src/hooks/__tests__/useTheme.test.ts
@@ -1,22 +1,23 @@
 import { act, renderHook } from '@testing-library/react-native';
-import { useTheme } from '~/hooks';
+import { Appearance } from 'react-native';
+
+import { useTheme, defaultThemeName } from '~/hooks';
 
 describe('Testing useTheme hook', () => {
-	it('should start with dark theme as default.', () => {
+	it("should start with the device's default theme, otherwise set the default dark theme.", () => {
 		const { result } = renderHook(() => useTheme());
-		expect(result.current.themeName).toBe("dark");
+		expect(result.current.themeName).toBe(Appearance.getColorScheme() || defaultThemeName);
 	});
 
-	it('should be able to change them from dark to light', () => {
+	it('should be able to change theme from dark to light', () => {
 		const { result } = renderHook(() => useTheme());
-		expect(result.current.themeName).toBe("dark");
 		act(() => {
 			result.current.changeTheme("light")
 		});
 		expect(result.current.themeName).toBe("light");
 	});
 
-	it('should set theme as dark if a wrong theme param was passed.', () => {
+	it('should set theme to dark if wrong theme parameter is passed.', () => {
 		const { result } = renderHook(() => useTheme());
 		act(() => {
 			result.current.changeTheme("light")
